cfa3bcd8dc3c33c3a3fd01398982e9d023c8f193
AtulShinde9552 <atul387659@gmail.com>
2024-07-26 11:25:45 +0530

    change middelware route

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from '@clerk/nextjs';
 // Protect specified routes, making them accessible only after login
 export default authMiddleware({
 
-  publicRoutes: ['/', '/api/webhook', '/tags', '/tags/:id', '/community', '/profile/:id'],
+  // publicRoutes: ['/', '/api/webhook', '/tags', '/tags/:id', '/community', '/profile/:id'],
   // ignoredRoutes: ['/api/webhook'],
 
 });
